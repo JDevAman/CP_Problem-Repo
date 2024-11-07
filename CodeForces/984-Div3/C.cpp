afcf8d5a7850bc92774a83a6138934adb3c7b858
Haribhakt <aman021276@gmail.com>
2024-11-07 12:42:34 +0530

    Added Starters 159

--- CodeForces/984-Div3/C.cpp
+++ CodeForces/984-Div3/C.cpp
@@ -1,9 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
+#define int long long int
+#define vi vector<int>
+#define si set<int>
+#define mi map<int,int>
+#define pi pair<int, int>
+# define yes cout<<"Yes"<<endl
+# define no cout<<"No"<<endl
+
+// Returns array of Primes in O n * (log(logn))
+// vector<int> sieve(int maxN) {
+//     vector<bool> isPrime(maxN + 1, true);
+//     isPrime[0] = isPrime[1] = false;
+//     for (int i = 2; i * i <= maxN; ++i) {
+//         if (isPrime[i]) {
+//             for (int j = i * i; j <= maxN; j += i) {
+//                 isPrime[j] = false;
+//             }
+//         }
+//     }
+//     vector<int> primes;
+//     for (int i = 2; i <= maxN; ++i) {
+//         if (isPrime[i]) {
+//             primes.push_back(i);
+//         }
+//     }
+//     return primes;
+// }
 
 void haribhakt() {
     ios_base::sync_with_stdio(false);
